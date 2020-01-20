6f40fff911f0ab73ee1d2276269f59ea7344c259
Elliott <elliottcost@gmail.com>
2020-01-19 22:16:31 -0500

    favicon

--- partials/_meta.php
+++ partials/_meta.php
@@ -2,4 +2,4 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link href="./css/styles.css" rel="stylesheet">
-<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+<link rel="shortcut icon" type="image/png" href="./images/favicon.png">
